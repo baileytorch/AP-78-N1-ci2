df156dad48d91d89a1fc34f03457028fd4af2b79
baileytorch <baileytorch@hotmail.com>
2025-06-09 19:47:47 -0400

    Clase 1, tipos de datos

---
+++ tipos_datos/datos_simple.py
@@ -0,0 +1,31 @@
+# Datos de tipo cadena de texto
+# string
+cadena_de_texto_1 = 'Cadena de texto'
+cadena_de_texto_2 = "Otra cadena de texto"
+cadena_de_texto_3 = """Este es una tercera 
+cadena de caracteres
+de múltiples líneas"""
+
+print('Esto es una cadena de texto')
+print('Esto es una cadena de texto con "comillas dobles"')
+print("Esto es una cadena de texto con 'comillas simples'")
+
+# Datos de tipo numérico
+# int
+numero_entero = 49
+# float
+numero_decimal = 56.32
+
+# Datos de tipo lógico
+# bool
+dato_verdadero = True
+dato_falso = False
+
+print(cadena_de_texto_1)
+print(cadena_de_texto_2)
+print(cadena_de_texto_3)
+
+saludo = "Buen día"
+nombre = input("Escriba su nombre: ")
+texto_final = saludo + nombre
+print(texto_final)
