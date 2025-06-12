2a702bb6cbd11319324b2eaed1c7b294b1f10935
baileytorch <baileytorch@hotmail.com>
2025-06-11 21:13:32 -0400

    Cambios en tipos de datos

---
+++ tipos_datos/datos_compuestos.py
@@ -0,0 +1,76 @@
+# Datos de tipo COMPUESTO o COMPLEJO
+# Son colecciones de datos de tipo simple o compuesto
+
+# LISTAS
+# Una lista es una colección ORDENADA y MUTABLE de elementos de cualquier tipo de datos,
+# los elementos se separan por comas.
+
+lista_1 = ['cadenas de texto',49,True,34.89,[1,2,3,4,5]]
+print(type(lista_1))
+print(len(lista_1))
+print(lista_1)
+print(lista_1[0])
+
+lista_1[1] = 'Aquiles Baeza'
+print(lista_1)
+
+# Agregar elementos a una lista
+lista_1.append(False)
+print(lista_1)
+
+# Quitar elementos de una lista
+# Quitamos el último elemento
+lista_1.pop()
+print(lista_1)
+
+# Quitamos un elemento por su índice
+lista_1.pop(4)
+print(lista_1)
+
+lista_1.insert(2,'test')
+lista_1.insert(0,56)
+print(lista_1)
+
+# Vaciamos completamente la lista
+lista_1.clear()
+print(lista_1)
+print('¿Dónde está la lista?')
+
+# DICCIONARIOS
+# Un diccionario es una colección ORDENADA y MUTABLE de PARES elementos de cualquier tipo de datos,
+# los elementos se separan por comas.
+
+diccionario_1 = {
+    'nombre':'Erick',
+    'apellido':'Bailey',
+    'Profesor':True,
+    'Edad':49,
+    'Género':'Masculino'
+}
+print(type(diccionario_1))
+print(diccionario_1)
+
+# Accediendo a los datos mediante la CLAVE
+print(diccionario_1['nombre'])
+print(diccionario_1['apellido'])
+
+# Elimando el último elemento
+diccionario_1.popitem()
+
+# Eliminar un elemento mediante su CLAVE
+diccionario_1.pop('Edad')
+print(diccionario_1)
+
+# Agregar elementos añadiendo una CLAVE y un VALOR
+diccionario_1['ciudad'] = 'Temuco'
+diccionario_1['Edad'] = 35
+print(diccionario_1)
+
+print(diccionario_1.items())
+print(diccionario_1.keys())
+print(diccionario_1.values())
+
+# Vaciar un diccionario
+diccionario_1.clear()
+print(diccionario_1)
+
