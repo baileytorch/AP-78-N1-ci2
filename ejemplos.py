49f470d4be8e299f940cbb5835cab8c647ffc2e2
baileytorch <baileytorch@hotmail.com>
2025-06-30 19:48:44 -0400

    Control de errores en Python

--- ejemplos.py
+++ ejemplos.py
@@ -1,2 +1,11 @@
-for i in range(10):
-    print(i)
+try:
+    a=int(input('Ingrese numerador: '))
+    b=int(input('Ingrese denominador: '))
+    division = a/b
+    print(division)
+except ZeroDivisionError:
+    print('No se puede dividir por 0.')
+except ValueError:
+    print('Valor ingresado NO corresponde.')
+except:
+    print('Error inesperado...')
---
+++ ejercicios/ejrcicio4.py
@@ -0,0 +1,2 @@
+# Crear una función que permita convertir temperatura de kelvin a celsius
+# C° = K - 273.15
